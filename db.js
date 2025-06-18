5299e4af1c05537ec997a338fe2532dedd33c649
DavidSilva15 <147518658+DavidSilva15@users.noreply.github.com>
2025-06-18 20:05:01 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'pelada71'
 });
 
 connection.connect((err) => {
